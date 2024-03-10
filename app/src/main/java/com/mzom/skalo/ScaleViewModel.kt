710a48fb0d5990b9cfb0dc02841bed6d08bda14e
HP <saarav354@gmail.com>
2024-03-10 23:07:58 +0530

    final

---
+++ app/src/main/java/com/mzom/skalo/ScaleViewModel.kt
@@ -0,0 +1,11 @@
+package com.mzom.skalo
+
+import androidx.lifecycle.ViewModel
+
+class ScaleViewModel : ViewModel() {
+
+    // TODO Support multiple measurements
+    // Today's measurement
+    var measurement: Measurement? = null
+
+}
